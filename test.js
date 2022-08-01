52d9eb33acde699fd0825b56f1df2e431fb76c4f
Dora <ohya721@gmail.com>
2022-08-01 23:06:26 +0800

    0801-1

--- test.js
+++ test.js
@@ -1,33 +1,9 @@
-// let arr = [80, 50, 10, 20, 5];
-// let arr2 = ['Fei', 'Bai', 'Han', 'Chu', 'Bubu'] ;
+function add(num) {
+  function fun(x = 0) {
+    return num + x
+  }
+  return fun
+}
 
-// forEach不會產生新陣列，如果賦值也會undefined
-// arr.forEach((item, idx) => {
-//   if(item == 80) return
-//   console.log(item);
-// });
-
-// map會產生新陣列，並可賦值到新變數
-// newArr = arr.map((item, idx) => {
-//   return item * item;
-// });
-
-// arr.splice(0, 1, 1314);
-
-// newArr = arr.filter(item => item <= 10);
-
-// for (let value of arr2) {
-//   value += ' <3' ;
-//   console.log(value);
-// }
-let num = 5
-// newmun = num % 5 // 取餘數
-newnum = num % 1
-
-// for(let i = 1; i <= 5; i++) {
-//   setTimeout(() => {
-//     console.log(i);
-//   }, 1000 * i)
-// }
-
-console.log(newnum);
+let addFive = add(5) // 5
+console.log(addFive(8)); // 13
