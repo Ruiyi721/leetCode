d8d185015424b04286ec6c0a06b1c95cd068cff3
Dora <ohya721@gmail.com>
2022-10-04 18:13:53 +0800

    等冪算秒數

---
+++ test_1.js
@@ -0,0 +1,23 @@
+let arr = [ 420, 1280, 12850, 100000 ]; // default 30s
+let sec = 30; // sec += 100s
+
+let basicWidth = 1000;
+// 每差10000長度 即...S
+// 1000w...100s +100s 10 3次
+// 10000w...200s +100s  10 4次
+// 100000w...300s +100s  10 5次
+// 值大於10的3次就加100秒 大於4次方再加100秒 大於5次方再加100秒
+
+// 找到值為10的幾次方的function
+function test(x, y) {
+    return Math.round(Math.log(y) / Math.log(x));
+}
+
+arr.forEach((e) => {
+    if (e >= basicWidth) {
+        let time = test(10, e);
+        if (time >= 3) sec += 100;
+    }
+})
+
+console.log(sec);
